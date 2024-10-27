c04bee1b80f40edd53c204cf03a706bebb2389ad
tarunsinghofficial <tarunsinghwap7@gmail.com>
2024-10-27 13:19:13 +0530

    refactor: moved mobile responsive code to MobileMenu.svelte component

--- src/components/navigation/Header.svelte
+++ src/components/navigation/Header.svelte
@@ -6,7 +6,7 @@
 	} from '$env/static/public';
 
 	import ThemeSwitcher from '$lib/ThemeSwitch/ThemeSwitcher.svelte';
-	import { fly } from 'svelte/transition';
+	import MobileMenu from './MobileMenu.svelte';
 
 	let isMobileMenuOpen = false;
 
@@ -74,45 +74,11 @@
 </div>
 
 {#if isMobileMenuOpen}
-	<div
-		class="fixed inset-0 z-50 flex flex-col items-center justify-center space-y-6 bg-white p-4 dark:bg-black md:hidden"
-		transition:fly={{ x: 1000, duration: 300 }}
-	>
-		<button on:click={toggleNavbar}>
-			<svg
-				class="close-icon h-6 w-6 text-gray-900 dark:text-white"
-				fill="none"
-				stroke="currentColor"
-				viewBox="0 0 24 24"
-				xmlns="http://www.w3.org/2000/svg"
-			>
-				<path
-					stroke-linecap="round"
-					stroke-linejoin="round"
-					stroke-width="2"
-					d="M6 18L18 6M6 6l12 12"
-				></path>
-			</svg>
-		</button>
-
-		<div class="flex flex-col items-center gap-4">
-			{#each Object.entries(headerLinks) as [key, value]}
-				<a
-					href={value}
-					class="block text-xl font-semibold text-gray-900 dark:text-white"
-					on:click={toggleNavbar}>{key}</a
-				>
-			{/each}
-		</div>
-		<div>
-			<ThemeSwitcher />
-		</div>
-	</div>
+	<MobileMenu {headerLinks} closeMenu={toggleNavbar} />
 {/if}
 
 <style lang="postcss">
-	.burger-icon,
-	.close-icon {
+	.burger-icon {
 		cursor: pointer;
 	}
 </style>
