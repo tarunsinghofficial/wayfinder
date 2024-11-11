28a13663ce41e35a21c8a3bc8d10ee22325ca5a9
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-11-11 23:59:52 +0200

    feat: add ItineraryDetails component for displaying itinerary information

---
+++ src/components/trip-planner/ItineraryDetails.svelte
@@ -0,0 +1,19 @@
+<script>
+	import LegDetails from './LegDetails.svelte';
+	import { formatTime } from '$lib/formatters';
+	export let itinerary;
+	export let expandedSteps;
+	export let toggleSteps;
+</script>
+
+<div class="border-b border-gray-300 p-4">
+	<p>Duration: {Math.round(itinerary.duration / 60)} minutes</p>
+	<p>Start Time: {formatTime(itinerary.startTime)}</p>
+	<p>End Time: {formatTime(itinerary.endTime)}</p>
+
+	<div class="mt-4 space-y-4">
+		{#each itinerary.legs as leg, index}
+			<LegDetails {leg} {index} {expandedSteps} {toggleSteps} />
+		{/each}
+	</div>
+</div>
