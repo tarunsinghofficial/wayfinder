e2091a49985a92229f72107a61979235b4b73b05
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-11-15 19:14:13 +0200

    feat: localization trip planner

--- src/components/trip-planner/ItineraryDetails.svelte
+++ src/components/trip-planner/ItineraryDetails.svelte
@@ -1,6 +1,7 @@
 <script>
 	import LegDetails from './LegDetails.svelte';
 	import { formatTime } from '$lib/formatters';
+	import { t } from 'svelte-i18n';
 	export let itinerary;
 	export let expandedSteps;
 	export let toggleSteps;
@@ -8,15 +9,18 @@
 
 <div class="mb-4 flex items-center justify-between rounded-lg bg-gray-100 p-4 shadow-md">
 	<div class="text-center">
-		<p class="text-sm font-semibold text-gray-500">Duration</p>
-		<p class="text-lg font-bold text-gray-700">{Math.round(itinerary.duration / 60)} min</p>
+		<p class="text-sm font-semibold text-gray-500">{$t('trip.duration')}</p>
+		<p class="text-lg font-bold text-gray-700">
+			{Math.round(itinerary.duration / 60)}
+			{$t('time.min')}
+		</p>
 	</div>
 	<div class="text-center">
-		<p class="text-sm font-semibold text-gray-500">Start Time</p>
+		<p class="text-sm font-semibold text-gray-500">{$t('trip.start_time')}</p>
 		<p class="text-lg font-bold text-gray-700">{formatTime(itinerary.startTime)}</p>
 	</div>
 	<div class="text-center">
-		<p class="text-sm font-semibold text-gray-500">End Time</p>
+		<p class="text-sm font-semibold text-gray-500">{$t('trip.end_time')}</p>
 		<p class="text-lg font-bold text-gray-700">{formatTime(itinerary.endTime)}</p>
 	</div>
 </div>
