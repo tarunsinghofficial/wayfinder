ae45ba498c382a65b90527715ebc959d1c0c6499
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-11-19 13:54:38 -0800

    feat: add function to convert Unix timestamp to local time

--- src/lib/formatters.js
+++ src/lib/formatters.js
@@ -42,3 +42,11 @@ export function formatTime(dateString) {
 		hour12: true
 	});
 }
+
+export function convertUnixToTime(seconds) {
+	if (!seconds) return '';
+	const date = new Date(seconds * 1000);
+	const utcDate = new Date(date.toUTCString().slice(0, -4));
+	console.log(utcDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }));
+	return utcDate.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
+}
