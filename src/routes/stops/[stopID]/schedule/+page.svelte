1113e725f6e19986514af927b1efea30b93cf0de
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-11-20 14:22:48 -0800

    feat: Add fetch new schedules when select new date & improve the ui

--- src/routes/stops/[stopID]/schedule/+page.svelte
+++ src/routes/stops/[stopID]/schedule/+page.svelte
@@ -1,49 +1,95 @@
 <script>
 	import { page } from '$app/stores';
-	import { convertUnixToTime, formatTime } from '$lib/formatters.js';
-	import { Accordion, AccordionItem } from 'flowbite-svelte';
+	import LoadingSpinner from '$components/LoadingSpinner.svelte';
+	import ScheduleAccordionItem from '$components/schedules-for-stop/scheduleAccordionItem.svelte';
+	import { formatTime } from '$lib/formatters.js';
+	import { Accordion } from 'flowbite-svelte';
 	import { Datepicker } from 'flowbite-svelte';
 	import { onMount } from 'svelte';
+	import { isLoading } from 'svelte-i18n';
 
 	let selectedDate = new Date();
+	let prevSelectedDate = null;
 	let schedulesMap = new Map();
+	let routeReference = new Map();
 	let schedules = [];
-	let stopName = 'Stop Name';
-	let stopId;
+	let stopName = null;
+	let stopId = null;
+	let stopDirection = null;
 	let expandedItems = [];
+	let loading = true;
+	let emptySchedules = false;
 
 	$: stopId = $page.params.stopID;
 
-	async function fetchScheduleForStop(stopId) {
+	async function fetchScheduleForStop(stopId, date) {
 		try {
-			const response = await fetch(`/api/oba/schedule-for-stop/${stopId}`);
+			loading = true;
+			emptySchedules = false;
+			const response = await fetch(`/api/oba/schedule-for-stop/${stopId}?date=${date}`);
 			if (!response.ok) throw new Error('Failed to fetch schedule for stop');
 			const scheduleForStop = await response.json();
-			handleScheduleForStopResponse(scheduleForStop.data.entry);
+			handleScheduleForStopResponse(scheduleForStop.data);
 		} catch (error) {
 			console.error('Error fetching schedules:', error);
+		} finally {
+			loading = false;
 		}
 	}
 
 	function handleScheduleForStopResponse(scheduleForStop) {
 		schedulesMap.clear();
-		for (let routeSchedule of scheduleForStop.stopRouteSchedules) {
+		routeReference.clear();
+
+		if (!scheduleForStop.entry.stopRouteSchedules.length) {
+			emptySchedules = true;
+			schedules = [];
+			expandedItems = [];
+			return;
+		}
+
+		setStopDetails(scheduleForStop.references.stops[0]);
+		mapRoutes(scheduleForStop.references.routes);
+		processRouteSchedules(scheduleForStop.entry.stopRouteSchedules);
+
+		schedules = Array.from(schedulesMap.values());
+		expandedItems = schedules.map(() => false);
+	}
+
+	function mapRoutes(routes) {
+		for (let route of routes) {
+			routeReference.set(route.id, route);
+		}
+	}
+
+	function setStopDetails(stop) {
+		stopName = stop.name;
+		stopId = stop.id;
+		stopDirection = stop.direction;
+	}
+
+	function processRouteSchedules(routeSchedules) {
+		for (let routeSchedule of routeSchedules) {
 			let routeId = routeSchedule.routeId;
 			let stopRouteDirectionSchedules = routeSchedule.stopRouteDirectionSchedules;
 
 			stopRouteDirectionSchedules.forEach((directionSchedule) => {
 				const stopTimesGroupedByHour = groupStopTimesByHour(directionSchedule.scheduleStopTimes);
-				schedulesMap.set(routeId, {
-					tripHeadsign: directionSchedule.tripHeadsign,
+				const routeName = getRouteName(routeId, directionSchedule.tripHeadsign);
+
+				schedulesMap.set(routeName, {
+					tripHeadsign: routeName,
 					stopTimes: stopTimesGroupedByHour
 				});
 			});
 		}
-		schedules = Array.from(schedulesMap.values());
-		expandedItems = schedules.map((_, index) => false);
 	}
 
-	// TODO: ADD AM AND PM LOGIC
+	function getRouteName(routeId, tripHeadsign) {
+		const route = routeReference.get(routeId);
+		return `${route.shortName ?? route.longName} - ${tripHeadsign}`;
+	}
+
 	function groupStopTimesByHour(stopTimes) {
 		const grouped = {};
 		for (let stopTime of stopTimes) {
@@ -61,81 +107,87 @@
 		expandedItems = schedules.map(() => expand);
 	}
 
-	onMount(() => {
-		fetchScheduleForStop(stopId);
-	});
+	function toggleAccordion(index) {
+		expandedItems = expandedItems.map((item, i) => (i === index ? !item : item));
+	}
+
+	$: if (selectedDate && selectedDate !== prevSelectedDate) {
+		const formattedDate = selectedDate.toISOString().split('T')[0];
+		prevSelectedDate = selectedDate;
+		fetchScheduleForStop(stopId, formattedDate);
+	}
 </script>
 
-<div class="mx-auto max-w-7xl p-5">
-	<div class="mb-6 rounded-lg bg-gray-100 p-6 text-center shadow-lg">
-		<h1 class="text-3xl font-bold text-green-700">Stop Details</h1>
-		<p class="mt-2 text-lg text-gray-700">
-			<strong>Stop Name:</strong>
-			{stopName} | <strong>Stop ID:</strong>
-			{stopId}
-		</p>
-	</div>
-
-	<div class="flex flex-col gap-6 md:flex-row">
-		<div class="md:w-1/3">
-			<h2 class="mb-4 text-xl font-semibold text-gray-800">Select Date</h2>
-			<div class="rounded-lg border border-gray-300 bg-white p-4 shadow">
-				<Datepicker inline bind:value={selectedDate} />
-				<p class="mt-4 text-sm text-gray-600">
-					Selected date: {selectedDate ? selectedDate.toLocaleDateString() : 'None'}
-				</p>
-			</div>
-		</div>
-
-		<div class="flex flex-1 flex-col">
-			<h2 class="mb-4 text-2xl font-bold text-gray-800">Route Schedules</h2>
-
-			<div class="mb-4 flex gap-4">
-				<button
-					class="text-md rounded-lg bg-green-500 px-6 py-2 text-white shadow hover:bg-green-600 active:bg-green-700"
-					on:click={() => toggleAll(true)}
-				>
-					Show All Routes
-				</button>
-				<button
-					class="text-md rounded-lg bg-gray-500 px-6 py-2 text-white shadow hover:bg-gray-600 active:bg-gray-700"
-					on:click={() => toggleAll(false)}
-				>
-					Collapse All Routes
-				</button>
-			</div>
-
-			<div
-				class="flex-1 overflow-y-auto rounded-lg border border-gray-200 bg-white p-6 shadow-lg"
-				style="max-height: calc(100vh - 220px);"
+{#if loading || $isLoading}
+	<LoadingSpinner />
+{:else}
+	<div class="mx-auto max-w-7xl overflow-y-auto p-5" style="max-height: calc(100vh - 100px);">
+		<div class="mb-6 rounded-lg bg-gray-100 p-6 text-center shadow-lg">
+			<h1 class="text-3xl font-bold text-green-700">Stop Details</h1>
+			<p class="mt-2 text-lg text-gray-700">
+				<strong>Stop Name:</strong>
+				{stopName} | <strong>Stop ID:</strong>
+				{stopId}
+			</p>
+			<p class="mt-2 text-lg text-gray-700">
+				<strong>Direction:</strong>
+				{stopDirection}
+			</p>
+			<a
+				href={`/stops/${stopId}`}
+				class="mt-4 inline-block rounded-lg bg-green-500 px-4 py-2 text-white shadow hover:bg-green-600"
 			>
-				<Accordion>
-					{#each schedules as schedule, index (schedule.tripHeadsign)}
-						<AccordionItem open={expandedItems[index]}>
-							<span slot="header" class="-semibold text-lg text-gray-800">
-								{schedule.tripHeadsign}
-							</span>
-							{#each Object.entries(schedule.stopTimes) as [hour, times]}
-								<h3 class="mt-4 text-lg text-green-700">{hour}:00</h3>
-								<table class="mt-2 w-full table-auto border border-gray-200">
-									<thead class="bg-gray-100">
-										<tr>
-											<th class="border px-4 py-2 text-left">Arrival Time</th>
-										</tr>
-									</thead>
-									<tbody>
-										{#each times as stopTime}
-											<tr>
-												<td class="border px-4 py-2 text-center">{stopTime.arrivalTime}</td>
-											</tr>
-										{/each}
-									</tbody>
-								</table>
+				Click here for real-time information
+			</a>
+		</div>
+
+		<div class="flex flex-col gap-6 md:flex-row">
+			<div class="md:w-1/3">
+				<h2 class="mb-4 text-xl font-semibold text-gray-800">Select Date</h2>
+				<div class="rounded-lg border border-gray-300 bg-white p-4 shadow">
+					<Datepicker inline bind:value={selectedDate} />
+					<p class="mt-4 text-sm text-gray-600">
+						Selected date: {selectedDate ? selectedDate.toLocaleDateString() : 'None'}
+					</p>
+				</div>
+			</div>
+
+			<div class="flex flex-1 flex-col">
+				<h2 class="mb-4 text-2xl font-bold text-gray-800">Route Schedules</h2>
+
+				<div class="mb-4 flex gap-4">
+					<button
+						class="text-md rounded-lg bg-green-500 px-6 py-2 text-white shadow hover:bg-green-600 active:bg-green-700"
+						on:click={() => toggleAll(true)}
+					>
+						Show All Routes
+					</button>
+					<button
+						class="text-md rounded-lg bg-gray-500 px-6 py-2 text-white shadow hover:bg-gray-600 active:bg-gray-700"
+						on:click={() => toggleAll(false)}
+					>
+						Collapse All Routes
+					</button>
+				</div>
+
+				<div
+					class="flex-1 overflow-y-auto rounded-lg border border-gray-200 bg-white p-6 shadow-lg"
+				>
+					{#if emptySchedules}
+						<p class="text-center text-gray-700">No schedules available for the selected date.</p>
+					{:else}
+						<Accordion flush>
+							{#each schedules as schedule, index (schedule.tripHeadsign)}
+								<ScheduleAccordionItem
+									{schedule}
+									expanded={expandedItems[index]}
+									on:toggle={() => toggleAccordion(index)}
+								/>
 							{/each}
-						</AccordionItem>
-					{/each}
-				</Accordion>
+						</Accordion>
+					{/if}
+				</div>
 			</div>
 		</div>
 	</div>
-</div>
+{/if}
