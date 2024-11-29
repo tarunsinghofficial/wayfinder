460b9204e2acbca199957487f8ceed78f2113b80
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2024-11-29 03:46:12 +0200

    refactor: migrate TabLink component to use $props

--- src/components/tabs/TabLink.svelte
+++ src/components/tabs/TabLink.svelte
@@ -1,10 +1,17 @@
 <script>
-	export let current = false;
-	export let href;
+	/**
+	 * @typedef {Object} Props
+	 * @property {boolean} [current]
+	 * @property {any} href
+	 * @property {import('svelte').Snippet} [children]
+	 */
+
+	/** @type {Props} */
+	let { current = false, href, children } = $props();
 </script>
 
 <div class="tab-container__item" class:tab-container__item--active={current}>
 	<a {href} class="block">
-		<slot></slot>
+		{@render children?.()}
 	</a>
 </div>
