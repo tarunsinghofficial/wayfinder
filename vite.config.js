05751c4bc7cff36f0245db3fa8e5c8a8dcc3b493
tarunsinghofficial <tarunsinghwap7@gmail.com>
2025-01-15 20:18:14 +0530

    chore: configure test coverage using Vitest

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
 	plugins: [sveltekit()],
 	test: {
-		include: ['src/**/*.{test,spec}.{js,ts}']
+		include: ['src/**/*.{test,spec}.{js,ts}'],
+		coverage: {
+			provider: 'v8',
+			reportsDirectory: './coverage',
+			reporter: ['text', 'html'],
+			all: true,
+			exclude: ['node_modules'],
+		}
 	}
 });
