917ea7aef548cb4eefb54fdce8ce83bfad06e511
Ahmedhossamdev <ahmedhossamdev1@gmail.com>
2025-02-06 11:06:40 +0200

    Add survey API endpoints for fetching, submitting, and updating survey responses

---
+++ src/routes/api/oba/surveys/+server.js
@@ -0,0 +1,30 @@
+import { json } from '@sveltejs/kit';
+import { env } from '$env/dynamic/private';
+import { buildURL } from '$lib/urls.js';
+
+const REGION_PATH = `regions/${env.PRIVATE_REGION_ID}/`;
+
+export async function GET({ url }) {
+
+    const userId = url.searchParams.get('userId');
+
+    try {
+        const url = buildURL(env.PRIVATE_OBACO_API_BASE_URL, `${REGION_PATH}surveys.json`, {
+            user_id: userId
+        });
+
+
+        const response = await fetch(url);
+
+        if (!response.ok) {
+            throw new Error('Failed to fetch surveys');
+        }
+
+        const data = await response.json();
+
+        return json(data);
+    } catch (error) {
+        console.error('Error loading surveys:', error);
+        return json({ error: 'Failed to load surveys' }, { status: 500 });
+    }
+}
---
+++ src/routes/api/oba/surveys/update-survey/[id]/+server.js
@@ -0,0 +1,32 @@
+
+import { json } from '@sveltejs/kit';
+import { env } from '$env/dynamic/private';
+import { buildURL } from '$lib/urls.js';
+
+
+export async function POST({ request, params }) {
+    try {
+        const { id } = params;
+        const body = await request.text()
+
+        const url = buildURL(env.PRIVATE_OBACO_API_BASE_URL, `/surveys_responses/${id}`);
+
+        const response = await fetch(url, {
+        method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body
+        });
+
+        if (!response.ok) {
+            throw new Error('Failed to update survey response');
+        }
+
+        const data = await response.json();
+        return json(data);
+    } catch (error) {
+        console.error('Error updating survey response:', error);
+        return json({ error: error.message }, { status: 500 });
+    }
+}
